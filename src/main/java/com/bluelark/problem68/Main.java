9adf7320b3b4bd6990d8b12bb6d0fcc47f0945c1
Wei Wang <wang0109@gmail.com>
2017-09-13 20:16:24 -0700

    init

---
+++ src/main/java/com/bluelark/problem68/Main.java
@@ -0,0 +1,15 @@
+package com.bluelark.problem68;
+
+import org.apache.commons.collections4.CollectionUtils;
+
+import java.util.List;
+
+public class Main {
+    static boolean checkListString(List<String> a, List<String> b) {
+        return CollectionUtils.isEqualCollection(a, b);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("hi");
+    }
+}
